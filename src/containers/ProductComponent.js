4662ceff655b87b043711d8e5f0dd24a6f2becd7
kiran <gandikotakiran99@gmail.com>
2022-02-16 20:17:25 +0530

    displaying complete data in the component

--- src/containers/ProductComponent.js
+++ src/containers/ProductComponent.js
@@ -1,20 +1,34 @@
 import React from 'react';
 import {useSelector} from 'react-redux'
 
+
 const ProductComponent =() =>{
     const products = useSelector((state)=>state.allProducts.products );
     console.log(products);
-    // const {id,name} = products[0];
-    return(
-        <div className="four wide column">
+    
+    const renderList= products.map(product=>{
+        const {id,title,category,image,price} = product;
+        return(
+            <div className="four column wide" key={id}>
             <div className="ui link cards">
                 <div className="card">
-                    <div className="image"></div>
+                    <div className="image">
+                        <img src={image} alt={title} />
+                    </div>
                     <div className="content">
-                        <div className="header"></div>
+                        <div className="header">{title}</div>
+                        <div className="meta price">$ {price}</div>
+                        <div className="meta">{category}</div>
                     </div>
                 </div>
             </div>
+        </div>
+            
+        )
+    })
+    return(
+        <div>
+            {renderList}
         </div>
     )
 }
